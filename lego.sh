55b3a2370ad227f0f64236b91f9bf964b94a66e4
lvxnull2 <184518908+lvxnull2@users.noreply.github.com>
2024-10-28 02:58:06 +0100

    Undocker

--- lego.sh
+++ lego.sh
@@ -8,25 +8,10 @@ if [ -r .env ]; then
   set +a
 fi
 
-if hash podman 2>/dev/null; then
-  docker=podman
-else
-  docker=docker
-fi
-
 # Crash if the token is unset
 : "${CF_DNS_API_TOKEN}"
 
-exec $docker run \
-  --rm \
-  --pull always \
-  -e CF_DNS_API_TOKEN \
-  -e LEGO_PATH=/legodir \
-  -v certs:/certs \
-  -v ./data:/legodir \
-  -v ./install-cert.sh:/install-cert.sh \
-  --name lego \
-  goacme/lego:latest \
+exec lego \
   --email "${EMAIL}" \
   --accept-tos \
   --dns cloudflare \
